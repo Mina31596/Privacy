be326afeabea3b3f3dde2707a30d56d32cbc087e
Eldridge Jameson <jjones199x@gmail.com>
2021-11-13 20:34:43 -0500

    Prototype Code

--- Privacy_Game/__init__.py
+++ Privacy_Game/__init__.py
@@ -2,39 +2,81 @@
 from otree.api import *
 c = cu
 
-
+doc = '\na.k.a. Keynesian beauty contest.\nPlayers all guess a number; whoever guesses closest to\n2/3 of the average wins.\nSee https://en.wikipedia.org/wiki/Guess_2/3_of_the_average\n'
 class Constants(BaseConstants):
     name_in_url = 'Privacy_Game'
-    players_per_group = None
+    players_per_group = 6
     num_rounds = 10
+    question_stack = ('Are you attracted to anyone in the game?', 'Have you ever been arrested?', 'Can you imagine pursuing a political career?')
+    players_per_grou = 0
     instructions_template = 'Privacy_Game/instructions.html'
-
 class Subsession(BaseSubsession):
     player_ranking = models.IntegerField()
-
 def display_question(group):
     session = group.session
     import random
     random.shuffle(session.quesstion_bank)
     while(session.quesstion_bank[0] in session.used_questions):
         random.shuffle(session.quesstion_bank)
+    
+    
     group.current_question = session.quesstion_bank[0]
     session.used_questions.append(session.quesstion_bank[0])
     print(group.current_question)
-
-#functions still needed to be complete
 def calculate_rewards(group):
-    pass
+    answer_yeses_count = 0
+    for player in group.get_players():
+        if player.participant.vars["answer"] == True:
+            answer_yeses_count += 1
+    
+    
+    
+    
 def get_total_yes(group):
     pass
 def update_player_ranking(group):
     pass
-
-
+def consolidate_question_list(group):
+    import copy
+    import random
+    
+    
+    questions = copy.deepcopy(Constants.question_stack)
+    additional_questions = []
+    
+    for player in group.get_players():
+        additional_questions.append(player.participant.vars["added_questions"])
+    
+    consolidated_list = list(questions) + additional_questions
+    print(consolidated_list)
+    
+    random.shuffle(consolidated_list)
+    #print(consolidated_list)
+    pick_from = [i for i in range(len(consolidated_list))]
+    
+    order_of_questions = []
+    for i in range(len(consolidated_list)):
+        pick = random.choice(pick_from)
+        if pick not in order_of_questions:
+            order_of_questions.append(pick)
+        #pick_from.pop(pick)
+    
+    for player in group.get_players():
+        player.participant.vars["list_questions"] = consolidated_list
+        player.participant.vars["order_of_questions"] = order_of_questions
+    
+    
+    
+    
+    print("pick",pick)
+    
+    print("additional",additional_questions)
+    print("consolidated",consolidated_list)
 class Group(BaseGroup):
     current_question = models.StringField()
     total_yeses = models.IntegerField()
-
+def my_function(player):
+    pass
 class Player(BasePlayer):
     id_inGroup = models.IntegerField(label='Please enter the ID given by the host')
     answer = models.BooleanField(choices=[[True, 'Yes'], [False, 'No']], label='')
@@ -43,28 +85,56 @@ class Player(BasePlayer):
     host = models.BooleanField()
     my_field = models.StringField()
     enter_question = models.LongStringField(blank=True, label='Enter question you would like to ask the group')
-    guess = models.IntegerField(label='')
-
+    guess = models.IntegerField(label='', max=Constants.players_per_group, min=0)
 class Wait_page_1(WaitPage):
-    after_all_players_arrive = display_question
-
+    after_all_players_arrive = consolidate_question_list
 class Answer_Question_Page(Page):
     form_model = 'player'
     form_fields = ['answer']
-
+    @staticmethod
+    def vars_for_template(player):
+        participant = player.participant
+        #import random
+        
+        
+        display_question_lis = player.participant.vars["list_questions"]
+        
+        #question_num = player.participant.vars["order_of_questions"]
+        
+        
+        print(display_question_lis)
+        print(player.round_number-1)
+        
+        
+        #display_question = display_question_lis[question_num[player.round_number-1]]
+        display_question = display_question_lis[0]
+        print(player.participant.vars["order_of_questions"])
+        #player.participant.vars["order_of_questions"].pop(0)
+        #print(player.participant.vars["order_of_questions"])
+        
+        player.participant.vars["list_questions"].pop(0)
+        return dict([('question', display_question)])
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        participant = player.participant
+        participant.vars["answer"] = player.answer
+        
 class Wait_page_2(WaitPage):
     after_all_players_arrive = get_total_yes
-
 class Guess_page(Page):
     form_model = 'player'
     form_fields = ['guess']
-
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        participant = player.participant
+        participant.vars["guess"] = player.guess
+        
+        
+        
 class Wait_page_3(WaitPage):
     after_all_players_arrive = calculate_rewards
-
 class Round_results(Page):
     form_model = 'player'
-
 class Final_results(Page):
     form_model = 'player'
     @staticmethod
@@ -72,6 +142,4 @@ class Final_results(Page):
         session = player.session
         subsession = player.subsession
         return subsession.round_number==10
-
-
 page_sequence = [Wait_page_1, Answer_Question_Page, Wait_page_2, Guess_page, Wait_page_3, Round_results, Final_results]
--- Privacy_Initialization/__init__.py
+++ Privacy_Initialization/__init__.py
@@ -5,43 +5,58 @@ c = cu
 doc = ''
 class Constants(BaseConstants):
     name_in_url = 'Privacy_Initialization'
-    players_per_group = None
+    players_per_group = 6
     num_rounds = 1
-
+    questions = ('Are you attracted to anyone in the room?', 'Have you ever been arrested?', 'Can you imagine pursuing a political career?')
 def Update_Player_Ranking(subsession):
     pass
-
 class Subsession(BaseSubsession):
     pass
-
 def displayQuestion(group):
-    session = group.session
     import random
-    random.shuffle(session.quesstion_bank)
-    group.current_question = session.quesstion_bank[0]
+    import copy
+    
+    questions = copy.deepcopy(Constants.question_stack)
+    #now append the players questions here
+    
+    #then suffle
+    random.shuffle(questions)
+    #group.current_question = questions[0]
     
 def getTotalYes(group):
     pass
-
 def calculateRewards(group):
     pass
-
 class Group(BaseGroup):
-    current_question = models.StringField()
-
+    answer_yeses = models.IntegerField(initial=0)
+    number_of_players = models.IntegerField(initial=0)
 def enterQuestion(player):
     session = player.session
+    group = player.group
+    import random
+    
+    random.shuffle(session.quesstion_bank)
+    group.current_question = session.quesstion_bank[0]
+    
     session.quesstion_bank = ["question1","question2","question3","question 4","question 5","question 6","question 7","question8","question9"]
     session.used_questions=[" "]
     session.quesstion_bank.append(player.enter_question)
-
+def determine_valid_id(player):
+    session = player.session
+    group = player.group
+    participant = player.participant
+    p_host= group.get_player_by_id(1)
+    session = player.session
+    session.invalid_Ids =[]
+    if player.id_InGroup != p_host.host_Id:
+        session.invalid_Ids.append(player.id_in_group)
+    participant.is_dropout= False
 class Player(BasePlayer):
-    id_InGroup = models.IntegerField()
+    id_InGroup = models.IntegerField(initial=0)
     name = models.StringField()
     host = models.BooleanField(choices=[[True, 'Yes'], [False, 'No']], initial=False, label=' ')
     host_Id = models.IntegerField()
-    enter_question = models.LongStringField(label='Ask a question for everyone to answer.')
-
+    enter_question = models.LongStringField(blank=True, label='Enter a question you would like to ask the group here:')
 class Welcome_Page(Page):
     form_model = 'player'
     form_fields = ['host']
@@ -50,30 +65,38 @@ class Welcome_Page(Page):
         import random
         num = random.randint(0,189753)
         player.host_Id = num
-
 class Host_Session(Page):
     form_model = 'player'
     @staticmethod
     def is_displayed(player):
         return player.host==True
-
 class Join_session(Page):
     form_model = 'player'
     form_fields = ['id_InGroup']
     @staticmethod
     def is_displayed(player):
         return player.host==False
-
-class WaitPage1(WaitPage):
-    body_text = 'Waiting for all the players to join'
-
-class Advertisement(Page):
+    @staticmethod
+    def before_next_page(player, timeout_happened):
+        determine_valid_id(player)
+class Invalid_Id(Page):
     form_model = 'player'
-
-class Write_Question(Page):
+    form_fields = ['id_InGroup']
+    @staticmethod
+    def is_displayed(player):
+        session = player.session
+        group = player.group
+        check = player.id_in_group in session.invalid_Ids
+        return check
+class WaitPage1(WaitPage):
+    body_text = 'Waiting for all players to join '
+class Write_Questions(Page):
     form_model = 'player'
     form_fields = ['enter_question']
     @staticmethod
     def before_next_page(player, timeout_happened):
-        enterQuestion(player)
-page_sequence = [Welcome_Page, Host_Session, Join_session, WaitPage1, Advertisement, Write_Question]
+        participant = player.participant
+        participant.vars["added_questions"] = player.enter_question
+        
+        
+page_sequence = [Welcome_Page, Host_Session, Join_session, Invalid_Id, WaitPage1, Write_Questions]
